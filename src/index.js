148bf31571d02764fcb061348278b0b750e9f24e
jojo_G <sne_jojo_g@jojoG.local>
2022-05-23 22:20:05 -0700

    final draft-2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
